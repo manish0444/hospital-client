8546e1f59545cfa349dedb16114c1783c168aa13
manish0444 <manishbhandari0444@gmail.com>
2025-11-03 16:20:27 +0545

    image

--- app/page.tsx
+++ app/page.tsx
@@ -135,7 +135,7 @@ const Home = () => {
               >
                 <div className="relative overflow-hidden rounded-xl shadow-2xl">
                   <img
-                    src="https://images.unsplash.com/photo-1576091160399-112ba8d25d1f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+                    src="https://impeccabuild.com.au/medical-clinic-interior-design-ideas/"
                     alt="Physiotherapy Treatment"
                     className="w-full h-auto object-cover"
                   />
